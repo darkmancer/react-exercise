0d5ddd8b3e12f55eb4bb54053a87eb6f4c5522a5
darkmancer <guboychatt@gmail.com>
2022-05-11 14:46:45 +0700

    add comment

--- src/App.js
+++ src/App.js
@@ -28,3 +28,15 @@ export default function App() {
     </div>
   );
 }
+
+
+/* React Challenge
+  เขียน app.js ขึ้นมาเป็นแอพ เคาเต้อที่ประกอบไปด้วย
+  1.ฟีเจ้อเพิ่มจำนวนcount กด1ครั้งเพิ่ม1
+  2.ฟีเจ้อลดจำนวนcount กด1ครั้งลด1
+  3.ฟีเจ้อ ปุ่มกดซ่อนCount  toggleได้
+  4.ถ้าcount เพิ่มหรือลด ต้องแสดงที่ title ของ browserด้วย
+  5.ครั้งแรกที่เข้าหน้าเว็ปต้องAlert ว่า Welcome to COUNTER APP BITCH
+  6.ทุกครั้งที่ Toggle ต้องAlertว่า TOGGLED BITCH
+  7.ต่อAPI https://covid19.ddc.moph.go.th/api/Cases/today-cases-all เพื่อหยิบจำนวนผู้ติดโควิทมาแสดงแทนCount
+*/
