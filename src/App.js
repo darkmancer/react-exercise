8484d441e2e79010482b7d9ff95ce9ca012138f7
darkmancer <guboychatt@gmail.com>
2022-05-20 22:45:42 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,5 @@
-import React, { useState } from "react";
-import Button from "./components/Button";
-import "./assets/css/style.css";
-
-export default function App() {
-  const [count, setCount] = useState(0);
-
-  let incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  let decrementCount = () => {
-    setCount(count - 1);
-  };
-
-  return (
-    <div className="app">
-      <div>
-        <div class="count">
-          <h3>Count:</h3>
-          <h1>{count}</h1>
-        </div>
-        <div class="buttons">
-          <Button title={"-"} action={decrementCount} />
-          <Button title={"+"} action={incrementCount} />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-
-/* React Challenge
+/*
+ React Challenge
   เขียน app.js ขึ้นมาเป็นแอพ เคาเต้อที่ประกอบไปด้วย
   1.ฟีเจ้อเพิ่มจำนวนcount กด1ครั้งเพิ่ม1
   2.ฟีเจ้อลดจำนวนcount กด1ครั้งลด1
@@ -39,4 +8,5 @@ export default function App() {
   5.ครั้งแรกที่เข้าหน้าเว็ปต้องAlert ว่า Welcome to COUNTER APP BITCH
   6.ทุกครั้งที่ Toggle ต้องAlertว่า TOGGLED BITCH
   7.ต่อAPI https://covid19.ddc.moph.go.th/api/Cases/today-cases-all เพื่อหยิบจำนวนผู้ติดโควิทมาแสดงแทนCount
+  
 */
